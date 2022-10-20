e95fdd2090136b33ebd486977e4d31679c87bc69
David Blatcher <dv.bltchr@gmail.com>
2022-10-20 12:46:53 +0100

    strcuture

--- src/count2.ts
+++ src/count2.ts
@@ -12,32 +12,32 @@ const dateRange = {
   toDate: "2022-09-01",
 };
 
-const results: TagAndSectionCount[] = [];
-
-const getDataAndUse = async (tagAndSection: TagAndSection) => {
-  const result = await getCountForSectionAndTag({
-    ...tagAndSection,
-    ...dateRange,
-  });
-
-  const output = `${result.tagId},${result.sectionId},${result.count},${
-    result.error || "OK"
-  }\n`;
-
-  if (result.error) {
-    console.warn(output);
-    throw result.error;
-  }
-
-  results.push(result);
-  if (results.length % 10 === 0) {
-    console.log(results.length, Date.now());
-  }
-
-  appendFileSync(FILENAME, output);
-};
-
 const scheduleAndWaitIdle = async (list: TagAndSection[]) => {
+  const results: TagAndSectionCount[] = [];
+
+  const getDataAndUse = async (tagAndSection: TagAndSection) => {
+    const result = await getCountForSectionAndTag({
+      ...tagAndSection,
+      ...dateRange,
+    });
+
+    const output = `${result.tagId},${result.sectionId},${result.count},${
+      result.error || "OK"
+    }\n`;
+
+    if (result.error) {
+      console.warn(output);
+      throw result.error;
+    }
+
+    results.push(result);
+    if (results.length % 10 === 0) {
+      console.log(results.length, Date.now());
+    }
+
+    appendFileSync(FILENAME, output);
+  };
+
   // create a simple limiter using https://github.com/SGrondin/bottleneck
   const limiter = new Bottleneck({
     maxConcurrent: 100,
