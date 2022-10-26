9b6ce5e2d1772c4d4ee0fff8f232a303078eb5d6
David Blatcher <dv.bltchr@gmail.com>
2022-10-26 14:59:01 +0100

    get section names nicely

--- src/makeSummaryTableOld.ts
+++ src/makeSummaryTableOld.ts
@@ -1,90 +1,9 @@
 import { appendFileSync, writeFileSync } from "fs";
 import input from "../results/old-embeds-sections-may-22.json";
+import sectionCounts from "../data/nonZeroSectionCounts2022.json";
 
-type SectionName =
-  | "world"
-  | "australia-news"
-  | "football"
-  | "sport"
-  | "commentisfree"
-  | "us-news"
-  | "business"
-  | "politics"
-  | "uk-news"
-  | "lifeandstyle"
-  | "environment"
-  | "film"
-  | "music"
-  | "books"
-  | "tv-and-radio"
-  | "society"
-  | "artanddesign"
-  | "stage"
-  | "culture"
-  | "money"
-  | "food"
-  | "technology"
-  | "crosswords"
-  | "global-development"
-  | "education"
-  | "media"
-  | "news"
-  | "science"
-  | "fashion"
-  | "games"
-  | "theobserver"
-  | "inequality"
-  | "info"
-  | "law"
-  | "travel"
-  | "cities"
-  | "animals-farmed"
-  | "weather"
-  | "help"
-  | "theguardian";
-
-const sections: SectionName[] = [
-  "world",
-  "australia-news",
-  "football",
-  "sport",
-  "commentisfree",
-  "us-news",
-  "business",
-  "politics",
-  "uk-news",
-  "lifeandstyle",
-  "environment",
-  "film",
-  "music",
-  "books",
-  "tv-and-radio",
-  "society",
-  "artanddesign",
-  "stage",
-  "culture",
-  "money",
-  "food",
-  "technology",
-  "crosswords",
-  "global-development",
-  "education",
-  "media",
-  "news",
-  "science",
-  "fashion",
-  "games",
-  "theobserver",
-  "inequality",
-  "info",
-  "law",
-  "travel",
-  "cities",
-  "animals-farmed",
-  "weather",
-  "help",
-  "theguardian",
-];
+type SectionName = keyof typeof sectionCounts;
+const sections = Object.keys(sectionCounts) as SectionName[];
 
 type EmbedReport = {
   embedPath: string;
@@ -98,7 +17,7 @@ type EmbedReport = {
   >;
 };
 const CSV_OUTPUT_FILENAME = "./tables/old-embeds-may-22.csv";
-// const JSON_INPUT_FILENAME = "./results/old-embeds-may-22.json";
+
 
 const list = Object.values(input);
 
@@ -108,7 +27,7 @@ const buildHeader = (): string => {
 
 const buildLine = (item: EmbedReport): string => {
   const percentages = sections.map(
-    (section) => item.sections[section].percentage || '??'
+    (section) => item.sections[section].percentage || "??"
   );
 
   return `${item.embedPath},${percentages.join(",")}\n`;
@@ -119,3 +38,6 @@ writeFileSync(CSV_OUTPUT_FILENAME, buildHeader());
 list.forEach((item) => {
   appendFileSync(CSV_OUTPUT_FILENAME, buildLine(item));
 });
+
+console.log('created:', CSV_OUTPUT_FILENAME)
+console.log(list.length, 'data lines')
